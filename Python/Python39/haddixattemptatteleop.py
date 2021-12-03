31fd520a9366de2b82727b31ff247036113f3941
CringeCodin <74989150+CringeCodin@users.noreply.github.com>
2021-12-03 11:51:48 -0800

    Update haddixattemptatteleop.py

--- Python/Python39/haddixattemptatteleop.py
+++ Python/Python39/haddixattemptatteleop.py
@@ -1,16 +1,18 @@
+#note: this code does not work as of 12/3/21
+#Haddix Attempt at Teleop (for certification)
 import wpilib
 from wpilib.drive import DifferentialDrive
-class myRobot(TimedRobot):
+class myRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.flm = VictorSP(0)
-        self.blm = VictorSP(1)
-        self.frm = VictorSP(2)
-        self.brm = VictorSP(3)
-        self.left = SpeedControllerGroup(self.flm, self.blm)
-        self.right = SpeedControllerGroup(self.frm, self.brm)
+        self.flm = wpilib.VictorSP(0)
+        self.blm = wpilib.VictorSP(1)
+        self.frm = wpilib.VictorSP(2)
+        self.brm = wpilib.VictorSP(3)
+        self.left = wpilib.SpeedControllerGroup(self.flm, self.blm)
+        self.right = wpilib.SpeedControllerGroup(self.frm, self.brm)
         self.myRobot = DifferentialDrive(self.left, self.right)
         self.myRobot.setExpiration(0.1)
-        self.xbox = XboxController(0)
+        self.xbox = wpilib.XboxController(0)
     def teleopInit(self):
         self.myRobot.setSafetyEnabled(True)
     def teleopPeriodic(self):
