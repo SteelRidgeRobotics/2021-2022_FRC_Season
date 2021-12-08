93fc2407edd6681c99ec6a1e5fc312268f2e63ee
Steel Ridge <74989150+CringeCodin@users.noreply.github.com>
2021-12-07 17:11:10 -0800

    Create can_motor_controllers.py
    Added can_controller_project

---
+++ Python/Python39/can_motor_controllers.py
@@ -0,0 +1,48 @@
+import wpilib
+import wpilib.drive
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.fLeftMotor = wpilib.CANTalon(0) 
+        self.bLeftMotor = wpilib.CANTalon(1) 
+        self.fRightMotor = wpilib.CANTalon(2) 
+        self.bRightMotor = wpilib.CANTalon(3) 
+        
+        self.bLeftMotor.changeControlMode(wpilib.CANTalon.ControlMode.Follower)
+        self.bLeftMotor.set(0)
+        self.bRightMotor.changeControlMode(wpilib.CANTalon.ControlMode.Follower)
+        self.bRightMotor.set(2)
+        
+        self.fLeftMotor.reverseSensor(True)
+        self.fRightMotor.reverseSensor(True)
+        
+        self.fLeftMotor.reverseOutput(True)
+        self.fRightMotor.reverseOutput(True)
+        
+        self.fLeftMotor.setFeedbackDevice(QuadEncoder)
+        self.fRightMotor.setFeedbackDevice(QuadEncoder)
+        
+        self.fLeftMotor.getBrakeEnableDuringNeutral(True)
+        self.bLeftMotor.getBrakeEnableDuringNeutral(True)
+        self.fRightMotor.getBrakeEnableDuringNeutral(True)
+        self.bRightMotor.getBrakeEnableDuringNeutral(True)
+        
+        self.drive = wpilib.drive.DifferentialDrive(self.fLeftMotor, fRightMotor)
+        
+        self.timer = wpilib.Timer()
+    def autonomousInit(self):
+        self.timer.reset()
+        wpilib.SmartDashboard.putNumber("Left Encoder Raw", self.fLeftMotor.getSensorPosition())
+        wpilib.SmartDashboard.putNumber("Right Encoder Raw", self.fRightMotor.getSensorPosition())
+        wpilib.SmartDashboard.putNumber("Left Rotations", self.fLeftMotor.getSensorPosition()/360)
+        wpilib.SmartDashboard.putNumber("Right Rotations", self.fRightMotor.getSensorPosition()/360)
+        self.timer.start()
+        
+    def autonomousPeriodic(self):
+        if self.timer.get() <= 5.0:
+            self.drive.tankDrive(0.5, 0.5)
+    def teleopInit(self):
+    def teleopPeriodic(self):
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
