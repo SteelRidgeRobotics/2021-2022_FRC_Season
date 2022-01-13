e450f6dd3c60963235d6abd059b1926977804fe3
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-01-13 11:33:27 -0800

    Switched TalonFX objects to WPI_TalonFX
    This switch will make the motors compatible with drive functions like the tankDrive.

--- Python/Python39/can_motor_controllers.py
+++ Python/Python39/can_motor_controllers.py
@@ -2,8 +2,11 @@ import wpilib
 import ctre
 from wpilib.drive import DifferentialDirve
 
-class MyRobot(wpilib.TimedRobot):
-    # Will need to install CTRE library through robotpy package with python
+class MyRobot(wpilib.IterativeRobot):
+    # NOTE: There are 2048 units per revolution for the talon fx (which is integrated in the falcon 500)
+    #kSlotIdx = 0
+    #kPIDLoopIdx = 0
+    #kTimeoutMs = 10
     def robotInit(self):
         self.Stick = wpilib.Joystick(0)
         
@@ -12,42 +15,32 @@ class MyRobot(wpilib.TimedRobot):
         self.fRightMotor = ctre.WPI_TalonFX(2) 
         self.bRightMotor = ctre.WPI_TalonFX(3) 
         
+        #set up followers
+        self.fLeftMotor.set(ctre.TalonFXControlMode.PercentOutput, 0)
+        self.bLeftMotor.set(ctre.TalonFXControlMode.Follower, 0)
+        self.fRightMotor.set(ctre.TalonFXControlMode.PercentOutput, 0) 
+        self.bRightMotor.set(ctre.TalonFXControlMode.Follower, 2)
         
-        """
-        # Set Controll Modes & makes sure there is no movement
-        self.fLeftMotor.set(ctre.ControlMode.PercentOutput, 0)
-        self.bLeftMotor.set(ctre.ControlMode.Follower, 0)
-        self.fRightMotor.set(ctre.ControlMode.PercentOutput, 0) 
-        self.bRightMotor.set(ctre.ControlMode.Follower, 2)
+        # We can get positions by using the integrated sensor for autonomous code in the future
+        #self.fLeftMotor.configSelectedFeedbackSensor(ctre.TalonFXFeedbackDevice.IntegratedSensor, self.kPIDLoopIdx, self.kTimeoutMs)
+        #self.fRightMotor.configSelectedFeedbackSensor(ctre.TalonFXFeedbackDevice.IntegratedSensor, self.kPIDLoopIdx, self.kTimeoutMs)
         
-        # Configure Sensors
-        #.configSelectedFeedbackSensor(feedback device, pidIdx, timeoutMs)
-        # pidIdx: 0 for primary loop, 1 for auxillary
-        # timeoutMs: timeout value in ms. Will not check if 0, will send error message if exceeds timeout
-        self.fLeftMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, 0)
-        self.bLeftMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, 0)
-        self.fRightMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, 0)
-        self.bRightMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, 0)
-        
-        # Getting Sensor feedback
-        #ctre.TalonFXSensorCollection(motorController)
-        """
         
         self.timer = wpilib.Timer()
         self.drive = wpilib.drive.DifferentialDrive(self.fLeftMotor, self.fRightMotor)
-        """
-    def autonomousInit(self):
-        self.timer.reset()
-        wpilib.SmartDashboard.putNumber("Left Encoder Raw", self.fLeftMotor.getSensorPosition())
-        wpilib.SmartDashboard.putNumber("Right Encoder Raw", self.fRightMotor.getSensorPosition())
-        wpilib.SmartDashboard.putNumber("Left Rotations", self.fLeftMotor.getSensorPosition()/360)
-        wpilib.SmartDashboard.putNumber("Right Rotations", self.fRightMotor.getSensorPosition()/360)
-        self.timer.start()
-        
-    def autonomousPeriodic(self):
-        if self.timer.get() <= 5.0:
-            self.drive.tankDrive(0.5, 0.5)
-        """
+  
+#    def autonomousInit(self):
+#        self.timer.reset()
+#        wpilib.SmartDashboard.putNumber("Left Encoder Raw", self.fLeftMotor.getSensorPosition())
+#        wpilib.SmartDashboard.putNumber("Right Encoder Raw", self.fRightMotor.getSensorPosition())
+#        wpilib.SmartDashboard.putNumber("Left Rotations", self.fLeftMotor.getSensorPosition()/360)
+#        wpilib.SmartDashboard.putNumber("Right Rotations", self.fRightMotor.getSensorPosition()/360)
+#        self.timer.start()
+#        
+#    def autonomousPeriodic(self):
+#        if self.timer.get() <= 5.0:
+#            self.drive.tankDrive(0.5, 0.5)
+#        
     def disabledPeriodic(self):
         self.fLeftMotor.disable()
         self.bLeftMotor.disable()
