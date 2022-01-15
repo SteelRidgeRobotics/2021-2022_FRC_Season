b22a1b17eb27dcdb19ccc8027c0e19f671d41d9e
James <cringeysketches@gmail.com>
2022-01-15 15:38:07 -0800

    Update oi.py

--- Palpatine_2022/robot/oi.py
+++ Palpatine_2022/robot/oi.py
@@ -1,5 +1,10 @@
 import wpilib
 import commands
 class OI:
+    def __init__(self, robot):
+        self.robot = robot
+        self.init_xbox()
     def init_xbox(self):
         self.xbox = wpilib.XboxController(0)
+
+e = OI("woajh")
