9ebe918bd02ffa0b6896a30abcd7fbc3122fa62b
Rinard <steverinard@users.noreply.github.com>
2022-01-15 10:55:06 -0800

    Update talon_test.py

--- Python/Python39/talon_test.py
+++ Python/Python39/talon_test.py
@@ -9,43 +9,43 @@ class MyRobot(wpilib.TimedRobot):
    
 
     def robotInit(self):
-        self.fRight = ctre.WPI_TalonFX(0)
-        self.fLeft = ctre.WPI_TalonFX(1) 
-        self.bRight = ctre.WPI_TalonFX(2) 
-        self.bLeft = ctre.WPI_TalonFX(3) 
+        self.fLeft = ctre.WPI_TalonFX(0)
+        self.bLeft = ctre.WPI_TalonFX(1) 
+        self.fRight = ctre.WPI_TalonFX(2) 
+        self.bRight = ctre.WPI_TalonFX(3) 
         
         self.timer = wpilib.Timer()
 
     def disabledPeriodic(self):
-        self.fRight.disable()
         self.fLeft.disable()
-        self.bRight.disable()
         self.bLeft.disable()
+        self.fRight.disable()
+        self.bRight.disable()
         
     def autonomousInit(self):
         self.timer.start()  
 
     def autonomousPeriodic(self):
         if self.timer.get() <=5:
-            self.fRight.set(0.5)
+            self.fLeft.set(0.5)
         
         if self.timer.get() <=10:
-            self.fRight.set(0)
-            self.fLeft.set(0.5)
-            
-        
-        if self.timer.get() <=15:
             self.fLeft.set(0)
-            self.bRight.set(0.5)
-            
-        
-        if self.timer.get() <=20:
-            self.bRight.set(0)
             self.bLeft.set(0.5)
             
         
-        if self.timer.get() <= 25:
+        if self.timer.get() <=15:
             self.bLeft.set(0)
+            self.fRight.set(0.5)
+            
+        
+        if self.timer.get() <=20:
+            self.fRight.set(0)
+            self.bRight.set(0.5)
+            
+        
+        if self.timer.get() <= 25:
+            self.bRight.set(0)
 
 
 if __name__ == "__main__":
