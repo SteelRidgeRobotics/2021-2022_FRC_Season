a1d84d85f4d48177d6a135f90f86bc6a5f937baf
Steel Ridge Robotics <nwalker3.14159@gmail.com>
2022-01-15 14:01:08 -0800

    Adding Python file structure for Palpatine.

---
+++ Palpatine_2022/robot/robot.py
@@ -0,0 +1,15 @@
+import wpilib
+from commandbased import CommandBasedRobot
+
+from commands import AutonomousCommandGroup
+
+class MyRobot(CommandBasedRobot):
+
+    def robotInit(self):
+        '''Initialize things like subsystems'''
+
+        self.autonomous = AutonomousCommandGroup()
+
+
+    def autonomousInit(self):
+        self.autonomous.start()
