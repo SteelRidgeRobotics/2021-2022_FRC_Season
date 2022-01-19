103f154d4e6a62b0446773da00117c6d55f3df7e
Steel Ridge Robotics <nwalker3.14159@gmail.com>
2022-01-18 16:51:24 -0800

    Worked on functionality

--- Palpatine_2022/robot/robotcontainer.py
+++ Palpatine_2022/robot/robotcontainer.py
@@ -1,11 +1,32 @@
 import wpilib
 import commands2
 import constants
-import subsystem.drivetrain import Drivetrain()
+from wpilib import XboxController
+from subsystems.drivetrain import Drivetrain()
+from commands.drive_by_joystick import DriveByJoystick
 
 class RobotContainer:
-    def __init__(self, robot):
+    def __init__(self, robot) -> None:
         self.robot = robot
         self.init_controllers()
     def init_controllers(self):
-        self.xbox = wpilib.XboxController(constants.kdriverControllerPort)
+        self.driverController = wpilib.XboxController(constants.kdriverControllerPort)
+
+        # chooser
+        self.chooser = wpilib.SendableChooser()
+
+        # Add commands to autonomous command chooser
+        #self.chooser.setDefaultOption("Simple Auto", self.chooser)
+        #self.chooser.addOption("Complex Auto", self.chooser)
+
+        #wpilib.SmartDashboard.putData("Autonomous", self.chooser)
+
+        self.configureButtonBindings
+        
+        wpilib.drive.setDefaultCommand(
+            DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY, lambda: -self.driverController.getRightY)
+        )
+        def configureButtonBindings(self):
+
+        #def getAutonomousCommand(self) -> commands.,Command:
+        #    return self.chooser.getSelected()
--- Palpatine_2022/robot/subsystems/drivetrain.py
+++ Palpatine_2022/robot/subsystems/drivetrain.py
@@ -1,17 +1,51 @@
 import wpilib
+import constants
 import wpilib.command import Subsystem
 from wpilib.drive import DifferentialDrive
 import ctre
+from wpilib import XboxController
+from robotcontainer import RobotContainer
 
-class DriveTrain(Subsystem):
-    def __init__(self, robot):
-        super().__init__("drivetrain")
-        self.robot = robot
 
-        self.fLeftMotor = ctre.WPI_TalonFX(0)
-        self.bLeftMotor = ctre.WPI_TalonFX(1)
-        self.fRightMotor = ctre.WPI_TalonFX(2)
-        self.bRightMotor = ctre.WPI_TalonFX(3)
+class DriveTrain(commands2.SubsystemBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+        #initilize motors
+        self.frontLeft = ctre.TalonFX(constants.kfrontLeft)
+        self.backLeft = ctre.TalonFX(constants.kbackLeft)
+        self.frontRight = ctre.TalonFX(constants.kfrontLeft)
+        self.backRight = ctre.TalonFX(constants.kbackLeft)
+
+        #set followers
+        self.backLeft.follow(self.frontLeft)
+        self.backRight.follow(self.frontRight)
+
+        #reverse sensors
+        self.frontLeft.setSensorPhase(False)
+        self.backLeft.setSensorPhase(False)
+
+        #invert motors on right side
+        self.frontRight.setSensorPhase(True)
+        self.backRight.setSensorPhase(True)
+
+        self.frontRight.confingSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, constants.ktimeoutMs)
+        self.frontLeft.confingSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, constants.ktimeoutMs)
+
+        #set motors to brake mode
+        self.frontLeft.setNeutralMode(ctre.NeutralMode.Brake)
+        self.backLeft.setNeutralMode(ctre.NeutralMode.Brake)
+        self.frontRight.setNeutralMode(ctre.NeutralMode.Brake)
+        self.backRight.setNeutralMode(ctre.NeutralMode.Brake)
+
+        def userDrive(self, RobotContainer.driveController) -> None:
+            
+        """
+
+        self.fLeftMotor = ctre.WPI_TalonFX(constants.kfrontLeft)
+        self.bLeftMotor = ctre.WPI_TalonFX(constants.kbackLeft)
+        self.fRightMotor = ctre.WPI_TalonFX(constants.kfrontRight)
+        self.bRightMotor = ctre.WPI_TalonFX(constants.kbackRight)
 
         self.spcleft = wpilib.SpeedControllerGroup(self.fLeftMotor, self.bLeftMotor)
         self.spcright = wpilib.SpeedControllerGroup(self.fRightMotor, self.bRightMotor)
@@ -19,6 +53,7 @@ class DriveTrain(Subsystem):
 
         self.drive.setSafetyEnabled(True)
         self.drive.setExpiration(0.1)
+        """
 
     def disable_drive():
         self.fLeftMotor.disable()
