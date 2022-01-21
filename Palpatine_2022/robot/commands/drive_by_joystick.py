b13058b7076fc4eaffb91c4c27ce4ba1be24157d
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-01-21 07:39:38 -0800

    Update drive_by_joystick.py

--- Palpatine_2022/robot/commands/drive_by_joystick.py
+++ Palpatine_2022/robot/commands/drive_by_joystick.py
@@ -8,9 +8,11 @@ class DriveByJoystick(commands2.CommandBase):
     """
     This allows us to drive the robot with an xbox controller
     """
-    def __init__(self, drive: Drivetrain, left: float, right: float) -> None:
+    def __init__(self, drive: Drivetrain, driveController: RobotContainer.driveController) -> None:
         self.drive = drive
+        self.driveController = driveController
         self.addRequirements([self.drive])
+        self.addRequirements([self.driveController])
     
     #def initialize(self):
         # Called just before the command runs for the first time
@@ -18,17 +20,17 @@ class DriveByJoystick(commands2.CommandBase):
 
     def execute(self):
         # Called repeatedly when this command is scheduled to run
-        self.drive.userDrive(RobotContainer.driveController.getY()*-1 + RobotContainer.driveController.getX(), RobotContainer.driveController.getY()*-1 - RobotContainer.driveController.getX())
+        self.drive.userDrive(self.driveController.getY()*-1 + self.driveController.getX(), self.driveController.getY()*-1 - self.driveController.getX())
 
     def isFinished(self):
         # Make this return True when this command no longer needs to run execute()
         return False
     
-    #def end(self, message="Ended"):
+    def end(self, message="Ended"):
     # 
         # Called once after isFinished returns True
         # Stop the drivetrain from moving any further
-        #self.robot.drivetrain.drive_tank(0, 0)
+        self.drive.userDrive(0, 0)
 
     def interrupted(self):
         # Called when another command which requres one or more of the same subsystems is scheduled to run
