c26b8333a06e6c5946d487b5a94d050d92508fee
CleverDude88 <95452711+CleverDude88@users.noreply.github.com>
2022-01-26 10:59:10 -0800

    added left bumper

--- Palpatine_2022/robot/robotcontainer.py
+++ Palpatine_2022/robot/robotcontainer.py
@@ -30,7 +30,7 @@ class RobotContainer:
 
         #self.configureButtonBindings()  
         
-        self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY(), lambda: self.driverController.getRightBumper()))
+        self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY(), lambda: self.driverController.getRightBumper(), lambda: self.driverController.getLeftBumper()))
         #self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY()))
         # def configureButtonBindings(self):
         #     """
