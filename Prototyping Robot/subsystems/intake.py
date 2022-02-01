b92f1a82e34940a47d09287c71e03ddea137b060
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-01 07:53:58 -0800

    Create intake.py

---
+++ Prototyping Robot/subsystems/intake.py
@@ -0,0 +1,15 @@
+import wpilib
+import commands2
+import constants
+
+class Intake(commands2.SubsystemBase):
+  def __init__(self) -> None:
+    super().__init__()
+    
+    # initialize motors & solenoids
+    
+    # use percent output control mode for operating the intake
+    
+    # method to move motor in/out of the chassis
+    
+    # method to spin the rollers on intake
