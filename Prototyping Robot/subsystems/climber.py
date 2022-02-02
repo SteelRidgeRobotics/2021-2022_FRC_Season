ed0ea0b261186579cd3e2e99a54ba52743438f4b
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-02 08:44:11 -0800

    Update climber.py

--- Prototyping Robot/subsystems/climber.py
+++ Prototyping Robot/subsystems/climber.py
@@ -6,37 +6,37 @@ class Climber(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
 
-        self.climberMotor = ctre.TalonFX(constants.kclimber)
+        self.climberMotorShort = ctre.TalonFX(constants.kclimber)
         # config feedback sensor
-        self.climberMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, constants.ktimeoutMs)
+        self.climberMotorShort.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor, 0, constants.ktimeoutMs)
 
         # configs
-        self.climberMotor.configNominalOutputForward(0, constants.ktimeoutMs)
-        self.climberMotor.configNominalOutputReverse(0, constants.ktimeoutMs)
-        self.climberMotor.configPeakOutputForward(1, constants.ktimeoutMs)
-        self.climberMotor.configPeakOutputReverse(-1, constants.ktimeoutMs)
+        self.climberMotorShort.configNominalOutputForward(0, constants.ktimeoutMs)
+        self.climberMotorShort.configNominalOutputReverse(0, constants.ktimeoutMs)
+        self.climberMotorShort.configPeakOutputForward(1, constants.ktimeoutMs)
+        self.climberMotorShort.configPeakOutputReverse(-1, constants.ktimeoutMs)
 
-        self.climberMotor.selectProfileSlot(constants.kSlotIdx, constants.kPIDLoopIdx)
+        self.climberMotorShort.selectProfileSlot(constants.kSlotIdx, constants.kPIDLoopIdx)
 
-        self.climberMotor.config_kP(constants.kSlotIdx, constants.kP, constants.ktimeoutMs) 
-        self.climberMotor.config_kI(constants.kSlotIdx, constants.kI, constants.ktimeoutMs)
-        self.climberMotor.config_kD(constants.kSlotIdx, constants.kD, constants.ktimeoutMs)
-        self.climberMotor.config_kF(constants.kSlotIdx, constants.kF, constants.ktimeoutMs)
+        self.climberMotorShort.config_kP(constants.kSlotIdx, constants.kP, constants.ktimeoutMs) 
+        self.climberMotorShort.config_kI(constants.kSlotIdx, constants.kI, constants.ktimeoutMs)
+        self.climberMotorShort.config_kD(constants.kSlotIdx, constants.kD, constants.ktimeoutMs)
+        self.climberMotorShort.config_kF(constants.kSlotIdx, constants.kF, constants.ktimeoutMs)
 
-        self.climberMotor.configMotionCruiseVelocity(constants.kmotorCruiseVelocity, constants.ktimeoutMs)
+        self.climberMotorShort.configMotionCruiseVelocity(constants.kmotorCruiseVelocity, constants.ktimeoutMs)
 
-        self.climberMotor.setSelectedSensorPosition(0, constants.kPIDLoopIdx, constants.ktimeoutMs)
+        self.climberMotorShort.setSelectedSensorPosition(0, constants.kPIDLoopIdx, constants.ktimeoutMs)
 
         # zero sensor
-        self.climberMotor.setNeutralMode(ctre.NeutralMode.Brake)
+        self.climberMotorShort.setNeutralMode(ctre.NeutralMode.Brake)
 
 
     def useClimber(self, positionChange) -> None:
         self.positionChange = positionChange
-        self.climberMotor.set(ctre.TalonFXControlMode.MotionMagic, self.positionChange + self.climberMotor.getSelectedSensorPosition())
+        self.climberMotorShort.set(ctre.TalonFXControlMode.MotionMagic, self.positionChange + self.climberMotor.getSelectedSensorPosition())
         
     def zeroSensor(self) -> None:
-        self.climberMotor.setSelectedSensorPosition(0, 0, constants.ktimeoutMs)
+        self.climberMotorShort.setSelectedSensorPosition(0, 0, constants.ktimeoutMs)
 
     def isFullyRetracted(self) -> bool:
         self.climberPos = self.climberMotor.getSelectedSensorPosition()
