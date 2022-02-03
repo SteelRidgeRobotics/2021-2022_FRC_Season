ca1eb794eff910d7a75e6496ae0640ec8d087127
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-03 15:38:57 -0800

    Update restore_climber_to_default.py

--- Prototyping Robot/commands/restore_climber_to_default.py
+++ Prototyping Robot/commands/restore_climber_to_default.py
@@ -4,26 +4,34 @@ from subsystems.climber import Climber
 import constants
 
 class RestoreClimberToDefault(commands2.CommandBase):
-    def __init__(self, climber: Climber):
+    def __init__(self, climber: Climber, climberMotor: int):
         super().__init__()
 
         self.climber = climber
+        self.climberMotor = climberMotor
 
         self.addRequirements([self.climber])
 
     def execute(self) -> None:
-        wpilib.SmartDashboard.putNumber("   Climber Position - ", (self.climber.climberMotor.getSelectedSensorPosition()))
-        wpilib.SmartDashboard.putNumber("   Climber Velocity - ", (self.climber.climberMotor.getSelectedSensorVelocity()))
-
-        if self.climber.climberMotor.getSelectedSensorVelocity() < 100:
-            self.climber.useClimber(constants.kClimberRate)
-        else:
-            self.climber.useClimber(0.0)
+        wpilib.SmartDashboard.putNumber("   Climber Position - ", (self.climber.climberMotorShort.getSelectedSensorPosition()))
+        wpilib.SmartDashboard.putNumber("   Climber Velocity - ", (self.climber.climberMotorShort.getSelectedSensorVelocity()))
+        wpilib.SmartDashboard.putNumber("   Climber Position - ", (self.climber.climberMotorTilted.getSelectedSensorPosition()))
+        wpilib.SmartDashboard.putNumber("   Climber Velocity - ", (self.climber.climberMotorTilted.getSelectedSensorVelocity()))
+        
+        if self.climberMotor == 1:
+            if self.climber.climberMotorShort.getSelectedSensorVelocity() < 100:
+                self.climber.useShortClimber(constants.kClimberRate)
+            else:
+                self.climber.useShortClimber(0)
+                
+         if self.climberMotor == 2:
+            if self.climber.climberMotorTilted.getSelectedSensorVelocity() < 100:
+                self.climber.useTiltedClimber(constants.kClimberRate)
+            else:
+                self.climber.useTiltedClimber(0)
 
     def end(self, interrupted: bool) -> None:
-        self.climber.useClimber(0.0)
-        self.climber.zeroSensor()
-        
-    
+        self.climber.useClimber(0)
+       
     def isFinished(self) -> bool:
         return self.climber.isFullyRetracted() # this number may change but this is an estimate of the velocity of the falcon 500 when the climber is fully retracted
