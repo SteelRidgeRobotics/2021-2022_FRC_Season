653931c9a6a099f697e175820884ad811d0548e6
James <nwalker3.14159@gmail.com>
2022-02-05 13:26:28 -0800

    added solenoid vars

--- Prototyping Robot/subsystems/intake.py
+++ Prototyping Robot/subsystems/intake.py
@@ -6,7 +6,8 @@ class Intake(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
     # initialize motors & solenoids
-    
+        self.leftSolenoid = wpilib.Solenoid(constants.ksolenoidModuleType, constants.ksolenoidModuleType, constants.kintakeSolenoidLeftPort)
+        self.rightSolenoid = wpilib.Solenoid(constants.ksolenoidModuleType, constants.ksolenoidModuleType, constants.kintakeSolenoidRightPort)
     # use percent output control mode for operating the intake
     
     # method to move motor in/out of the chassis
