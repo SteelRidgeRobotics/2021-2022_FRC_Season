7b21cc720dcfb78b905cef21cc8fb89c6369dd69
Steel Ridge Robotics <nwalker3.14159@gmail.com>
2022-02-05 12:59:11 -0800

    All the things

--- Prototyping Robot/subsystems/intake.py
+++ Prototyping Robot/subsystems/intake.py
@@ -3,9 +3,8 @@ import commands2
 import constants
 
 class Intake(commands2.SubsystemBase):
-  def __init__(self) -> None:
-    super().__init__()
-    
+    def __init__(self) -> None:
+        super().__init__()
     # initialize motors & solenoids
     
     # use percent output control mode for operating the intake
