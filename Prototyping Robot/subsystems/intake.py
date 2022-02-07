10d0096fa5a9719d139b5ee86202d929e22755ba
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-07 07:39:56 -0800

    Update intake.py

--- Prototyping Robot/subsystems/intake.py
+++ Prototyping Robot/subsystems/intake.py
@@ -9,7 +9,9 @@ class Intake(commands2.SubsystemBase):
         self.leftSolenoid = wpilib.Solenoid(constants.ksolenoidModuleType, constants.ksolenoidModuleType, constants.kintakeSolenoidLeftPort)
         self.rightSolenoid = wpilib.Solenoid(constants.ksolenoidModuleType, constants.ksolenoidModuleType, constants.kintakeSolenoidRightPort)
         self.intakeMotor = ctre.TalonFX(constants.kintake)
-    # use percent output control mode for operating the intake
+    
+    
+    # method to move motor in/out of the chassis
     def pushIntakeOut(self) -> None:
         self.leftSolenoid.set(True)
         self.rightSolenoid.set(True)
@@ -17,9 +19,11 @@ class Intake(commands2.SubsystemBase):
     def pullIntakeIn(self) -> None:
         self.leftSolenoid.set(False)
         self.rightSolenoid.set(False)
-
+        
+        
+    # use percent output control mode for operating the intake
     def spinIntake(self, percentage: float) -> None:
         self.intakeMotor.set(ctre.TalonFXControlMode.PercentOutput, percentage)
-    # method to move motor in/out of the chassis
+    
     
     # method to spin the rollers on intake
