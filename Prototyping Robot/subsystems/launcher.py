6d76db082f51efbd78d5b65a1616c2f00efd813e
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-07 13:41:42 -0800

    Update launcher.py

--- Prototyping Robot/subsystems/launcher.py
+++ Prototyping Robot/subsystems/launcher.py
@@ -7,9 +7,16 @@ class Launcher(commands2.SubsystemBase):
         super().__init__()
     # initialize solenoids
         self.launcherSolenoid = wpilib.Solenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, constants.klauncherSolenoidPort)
+        self.is_up = False
 
     def launch(self) -> None:
         self.launcherSolenoid.set(True)
+        self.is_up = True
 
     def goDown(self) -> None:
         self.launcherSolenoid.set(False)
+        self.is_Up = False
+    
+    def isUp(self) -> bool:
+        return self.is_up
+        
