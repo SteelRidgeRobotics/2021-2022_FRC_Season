50c350413ab1472ff80fd464347d0f5986072323
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-07 07:55:50 -0800

    Update move_intake.py

--- Prototyping Robot/commands/move_intake.py
+++ Prototyping Robot/commands/move_intake.py
@@ -1,2 +1,26 @@
+import typing
 import commands2
 from subsystems.intake import Intake
+import wpilib
+
+class MoveIntake(commands2.CommandBase):
+    def __init__(self, intake: Intake, out: typing.Callable[[], bool]):
+        super().__init__()
+        self.intake = intake
+        self.out = out
+        self.done = False
+        self.addRequirements([self.intake])
+        
+    def execute(self) -> None:
+        if self.out:
+            self.intake.pushIntakeOut()
+            self.done = True
+        else:
+            self.intake.pullIntakeIn()
+            self.done = True
+      
+    def end(self) -> None:
+        """Some code here, if needed"""
+      
+    def isFinished(self) -> bool:
+        return self.done
