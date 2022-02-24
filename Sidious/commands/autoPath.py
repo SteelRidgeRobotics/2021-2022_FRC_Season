0ff76ae41e8e5c721b4a162daf6f0e1dacace8e1
steverinard <steverinard@users.noreply.github.com>
2022-02-23 18:25:25 -0800

    Update autoPath.py

--- Sidious/commands/autoPath.py
+++ Sidious/commands/autoPath.py
@@ -3,8 +3,9 @@ from setpathtodrive import SetPathToDrive
 
 class AutoPath(commands2.SequentialCommandGroup):
 
-    def __init__(self, setpath: SetPathToDrive):
-         super().__init__(
-             setpath.goPathA(SetPathToDrive, False),
-             setpath.goPathB(SetPathToDrive, True)
+    def __init__(self):
+        setpath = SetPathToDrive
+        super().__init__(
+             setpath.goPathA(setpath, False),
+             setpath.goPathB(setpath, True),
          )
