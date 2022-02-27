4f4f8a0c7f648e1f42a6e0079b88e54828d6c1ac
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-26 20:08:36 -0800

    Update constants.py

--- Captain Hook/constants.py
+++ Captain Hook/constants.py
@@ -1,1 +1,18 @@
+import math
 
+#Controller ports
+kdriverControllerPort = 0
+kfunctionsControllerPort = 1
+
+#Motors
+kfrontLeft = 0
+kbackLeft = 1
+kfrontRight = 2
+kbackRight = 3
+
+#Encoders
+ktimeoutMs = 10
+
+#Physical constants
+kunitsPerRotation = 2048.0
+kwheelCircumference = 1.57 #this is in feet
