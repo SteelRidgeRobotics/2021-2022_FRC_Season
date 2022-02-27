7c778998415f8c49a0c84c66306fef1bcd5259b3
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-02-26 20:20:04 -0800

    Update robotcontainer.py

--- Captain Hook/robotcontainer.py
+++ Captain Hook/robotcontainer.py
@@ -3,16 +3,23 @@ import commands2
 import constants
 import ctre
 from wpilib import XboxController
+
+
 from commands.drive_by_joystick import DriveByJoystick
-from commands.drive_straight import DriveStraight
 from subsystems.drivetrain import Drivetrain
 from commands2.button import JoystickButton
+
+from commands.climb_by_joystick import ClimbByJoystick
+
+from commands.drivetrain import Drivetrain
+from commands.climber import Climber
 
 #hi
 class RobotContainer:
     def __init__(self) -> None:
         # driver controller
         self.driverController = XboxController(constants.kdriverControllerPort)
+        self.
         
         self.frontLeft = ctre.TalonFX(constants.kfrontLeft)
         self.backLeft = ctre.TalonFX(constants.kbackLeft)
@@ -25,27 +32,28 @@ class RobotContainer:
         
         #subsystems
         self.drive = Drivetrain()
+        self.climber = Climber()
         
         # chooser
-        self.chooser = wpilib.SendableChooser()
+        #self.chooser = wpilib.SendableChooser()
         
         # Add commands to autonomous command chooser
-        self.driveStraight = DriveStraight(self.drive, constants.kdistanceToTravel)
-        self.chooser.setDefaultOption("Drive Straight", self.driveStraight)
+        #self.driveStraight = DriveStraight(self.drive, constants.kdistanceToTravel)
+        #self.chooser.setDefaultOption("Drive Straight", self.driveStraight)
 
-        wpilib.SmartDashboard.putData("Autonomous", self.chooser)
+        #wpilib.SmartDashboard.putData("Autonomous", self.chooser)
                 
         #self.configureButtonBindings() 
         
         # SINGLE JOYSTICK
         #self.drive.userDrive(self.driveController.getY()*-1 + self.driveController.getX(), self.driveController.getY()*-1 - self.driveController.getX()
-        
         # LEFT_JOY: FORWARD/BACKWARD; RIGHT_JOY: TURN
-        self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: (-self.driveController.getLeftY() + self.driveController.getRightX()), lambda: (-self.driveController.getLeftY() - self.driveController.getRightX()), lambda: self.driverController.getRightBumper(), lambda: self.driverController.getLeftBumper()))
-        
+        self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: (-self.driverController.getLeftY() + self.driverController.getRightX()), lambda: (-self.driverController.getLeftY() - self.driverController.getRightX()), lambda: self.driverController.getRightBumper(), lambda: self.driverController.getLeftBumper()))
         # TANK DRIVE (EACH SIDE IS CONTROLLED BY IT'S RESPECTABLE JOYSTICK)
         #self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY(), lambda: self.driverController.getRightBumper(), lambda: self.driverController.getLeftBumper()))
         
+        # climbing
+        self.climber.setDefaultCommand(ClimbByJoystick(self.climber, lambda: -self.functionsController.getLeftY(), lambda: -self.functionsController.getRightY()))
         
     #def configureButtonBindings(self):
 
