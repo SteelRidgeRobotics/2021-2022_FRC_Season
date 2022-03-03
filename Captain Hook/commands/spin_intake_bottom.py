eb7199494dadaa9f2f38a0ffc1d4d81fbdfbb1fa
James <nwalker3.14159@gmail.com>
2022-03-02 18:40:29 -0800

    Continuing to work on intake

---
+++ Captain Hook/commands/spin_intake_bottom.py
@@ -0,0 +1,21 @@
+import commands2
+from subsystems.intake import Intake
+
+class SpinIntakeBottom(commands2.CommandBase):
+    def __init__(self, intake: Intake, percent: float):
+        super().__init__()
+        
+        self.intake = intake
+        self.isDone = False
+        self.addRequirements([self.intake])
+    
+    def execute(self) -> None:
+        self.intake.spinIntakeBottom(1.0)
+        # may need to add some sort of wait here
+
+    def end(self) -> None:
+        self.intake.spinIntakeBottom(0.0)
+        self.isDone = True       
+    
+    def isFinished(self) -> bool:
+        return self.isDone
