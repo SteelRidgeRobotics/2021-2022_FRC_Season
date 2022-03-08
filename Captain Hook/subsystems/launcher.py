abbf1f14c6c566bcdd3bf0610fdc0c91c2d63015
Steel Ridge Robotics <nwalker3.14159@gmail.com>
2022-03-07 16:49:31 -0800

    ser;oiofgnvw3o;irjgvn ;owrjgvnoew[ijgvnlilewrijngv;ow;ririjmgvw gjliwrgvliw4ugt

--- Captain Hook/subsystems/launcher.py
+++ Captain Hook/subsystems/launcher.py
@@ -6,7 +6,7 @@ class Launcher(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
         # initialize solenoids
-        self.launcherSolenoid = wpilib.Solenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, *constants.klauncherSolenoidPorts)
+        self.launcherSolenoid = wpilib.Solenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, constants.klauncherSolenoidIn, constants.klauncherSolenoidOut)
         self.is_up = False
 
     def togglePosition(self) -> None:
