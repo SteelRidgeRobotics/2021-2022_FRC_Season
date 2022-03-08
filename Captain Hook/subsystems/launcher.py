304298832353c27280a9178440057073ac909c75
Steel Ridge Robotics <nwalker3.14159@gmail.com>
2022-03-07 16:29:02 -0800

    robot go pew pew

--- Captain Hook/subsystems/launcher.py
+++ Captain Hook/subsystems/launcher.py
@@ -6,16 +6,15 @@ class Launcher(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
         # initialize solenoids
-        self.launcherSolenoid = wpilib.Solenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, constants.klauncherSolenoidPort)
-        self.launcherSolenoidIn = wpilib.Solenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, constants.klauncherSolenoidPortIn)
+        self.launcherSolenoid = wpilib.DoubleSolenoid(constants.ksolenoidModule, constants.ksolenoidModuleType, *constants.klauncherSolenoidPorts)
         self.is_up = False
 
     def togglePosition(self) -> None:
         if self.is_up:
-            self.launcherSolenoidIn.set(True)
+            self.launcherSolenoid.set(wpilib.DoubleSolenoid.Value.kReverse)
         else:
-            self.launcherSolenoid.set(True)
-        self.is_up = True
+            self.launcherSolenoid.set(wpilib.DoubleSolenoid.Value.kForward)
+        self.is_up = not self.is_up
     
     def isUp(self) -> bool:
         return self.is_up
---
+++ Captain Hook/subsystems/vision.py
@@ -0,0 +1,5 @@
+import wpilib
+
+def cameraLaunch():
+    wpilib.CameraServer.launch()
+    
