83c21f5388d31c022be11b9b9537a54566d7e7a1
James <nwalker3.14159@gmail.com>
2022-03-08 15:06:07 -0800

    Path programming

--- Captain Hook/commands/multiple_paths.py
+++ Captain Hook/commands/multiple_paths.py
@@ -13,16 +13,18 @@ from subsystems.drivetrain import Drivetrain
 import constants
 
 #class for command
-class MoveWithTrajectory(commands2.SequentialCommandGroup):
+class MultiplePaths(commands2.SequentialCommandGroup):
     #init function with self and drivetrain. here's a fun fact: you don't need to use self, you can use any name.
     def __init__(self, drive: Drivetrain):
         #super init (initializes sequentialcommandgroup)
         super().__init__()
+
+        self.drive = drive
         
-        #voltage and feedforward (what is this)
+        #voltage and feedforward
         autoVoltageConstraint = DifferentialDriveVoltageConstraint(SimpleMotorFeedforwardMeters(constants.kS, constants.kA), constants.kdriveKinematics, maxVoltage=10)
 
-        #configure forward and reverse trajectories (what does this mean)
+        #configure forward and reverse trajectories
         forwardConfig = TrajectoryConfig(constants.kmaxVelocity, constants.kmaxAccel)
         forwardConfig.setKinematics(constants.kdriveKinematics)
         forwardConfig.addConstraint(autoVoltageConstraint)
@@ -34,3 +36,6 @@ class MoveWithTrajectory(commands2.SequentialCommandGroup):
         reverseConfig.setReversed(True)
 
         #this is nowhere near done
+        trajectory1p1 = TrajectoryGenerator.generateTrajectory(Pose2d(7.66, 2.63, Rotation2d(-112.25)), Pose2d(7.63, 1.09, Rotation2d(-90.00)))
+        trajectory2p1 = TrajectoryGenerator.generateTrajectory(Pose2d(7.66, 2.63, Rotation2d(-112.25)), Pose2d(5.80, 2.26, Rotation2d(-155.82)))
+        trajectory3p1 = TrajectoryGenerator.generateTrajectory(Pose2d(6.78, 4.75, Rotation2d(-158.39)), Pose2d(5.67, 5.85, Rotation2d(-149.68)))
