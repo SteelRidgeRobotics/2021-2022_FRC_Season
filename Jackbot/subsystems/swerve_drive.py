50dae7fed8bd9ae430c1d523de380bd9e3b1bb1c
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-04-13 12:49:57 -0700

    Update swerve_drive.py

--- Jackbot/subsystems/swerve_drive.py
+++ Jackbot/subsystems/swerve_drive.py
@@ -3,7 +3,20 @@ import ctre
 import constants
 import math
 
+class SwerveWheel():
+    #pid controller
+    #direction motor
+    #direction sensor
+    
 class SwerveDrive(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
         #init motors
+        #init encoders
+        
+        #need to create some way to convert from Talon FX units to degrees
+        # May try to make a file/method to use the position to do so
+        
+        #call gyro
+        
+        
