f0b5655aaf993a1802f6d607903d46b75823f840
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-04-18 07:54:19 -0700

    Update swerve_drive.py

--- Jackbot/subsystems/swerve_drive.py
+++ Jackbot/subsystems/swerve_drive.py
@@ -4,13 +4,22 @@ import constants
 import math
 import wpimath.controller.PIDController as PIDController
 class SwerveWheel():
-    def __init__(self, directionMotor, speedMotor):
+    def __init__(self, directionMotor: ctre.TalonFX, speedMotor: ctre.TalonFX, P: float, I: float, D: float):
+        # we assume that all the motors are the same: Falcon 500s
         self.pidController = PIDController
+        self.pidController.setPID(P, I, D)
+        self.directionMotor = directionMotor
+        self.speedMotor = speedMotor
         
-    def setPID(P, I, D):
-        self.pidController.setPID()
+    def setPID(self, P, I, D) -> None: # incase if we need to reset it, but we probably don't, so this may be deleted later
+        self.pidController.setPID(P, I, D)
     #pid controller
+    def setDirection(self, setpoint: float):
+        self.pidController.reset()
+        self.pidController.setSetpoint(setpoint)
+        error = self.pidController.calculate(directionMotor.getSelectedSensorPosition(), setpoint)
     #direction motor
+        
     #direction sensor
     
 class SwerveDrive(commands2.SubsystemBase):
