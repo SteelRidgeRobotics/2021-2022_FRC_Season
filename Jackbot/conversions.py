ac75fc2efdcd4c3c75f58bcaa4bffc0fa974378d
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-04-19 12:11:06 -0700

    Update conversions.py

--- Jackbot/conversions.py
+++ Jackbot/conversions.py
@@ -4,3 +4,11 @@ def convertDegreesToTalonFXUnits(float: num) -> float:
     conversionFactor = 2048/360
     num *= conversionFactor
     return num
+
+def sign(num) -> int:
+    if num > 0:
+        return 1
+    elif num < 0:
+        return -1
+    else:
+        return 0
