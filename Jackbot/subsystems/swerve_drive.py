27f1367d20aa0eee2eec35eec1ec8c16f412669f
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-04-19 07:55:41 -0700

    Update swerve_drive.py

--- Jackbot/subsystems/swerve_drive.py
+++ Jackbot/subsystems/swerve_drive.py
@@ -3,6 +3,7 @@ import ctre
 import constants
 import math
 import wpimath.controller.PIDController as PIDController
+import numpy
 class SwerveWheel:
     def __init__(self, directionMotor: ctre.TalonFX, speedMotor: ctre.TalonFX, P: float, I: float, D: float):
         # we assume that all the motors are the same: Falcon 500s
@@ -14,13 +15,28 @@ class SwerveWheel:
     def setPID(self, P, I, D) -> None: # incase if we need to reset it, but we probably don't, so this may be deleted later
         self.pidController.setPID(P, I, D)
     #pid controller
+    def closestAngle(self, a: float, b: float):
+        # this converts to angles
+        dir = float((b % 360.0) - (a % 360))
+        
+        if math.abs(dir) > 180.0:
+            dir = -(numpy.sign(dir) * 360.0) + dir
+        return dir
+    
     def setDirection(self, setpoint: float):
+        # use motion magic if able
+        #direction motor
+        self.directionMotor.set(ctre.TalonFXControleMode.MotionMagic, self.setpoint)
+        """
         self.pidController.reset()
         self.pidController.setSetpoint(setpoint)
         error = self.pidController.calculate(directionMotor.getSelectedSensorPosition(), setpoint)
-    #direction motor
-        
-    #direction sensor
+        output = 0
+        if not self.pidController.atSetpoint():
+            output = max(min(error, 1), -1)
+        # may have to be voltage instead of position
+        self.directionMotor.set(ctre.TalonFXControlMode.Position, output)
+        """
     
 class SwerveDrive(commands2.SubsystemBase):
     def __init__(self) -> None:
