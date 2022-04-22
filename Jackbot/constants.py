3261d756d9c3a5de898a815bd03a8ed93f89dc19
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-04-22 07:47:35 -0700

    Update constants.py

--- Jackbot/constants.py
+++ Jackbot/constants.py
@@ -4,3 +4,16 @@ from wpimath.kinematics import DifferentialDriveKinematics
 
 #controller ports
 kdriverControllerPort = 0
+
+# motors
+kleftFrontSpeedID = 0
+kleftRearSpeedID = 1
+krightFrontSpeedID = 2
+krightRearSpeedID = 3
+
+kleftFrontDirectionID = 4
+kleftRearDirectionID = 5
+krightFrontDirectionID = 6
+krightRearDirectionID = 7
+
+
