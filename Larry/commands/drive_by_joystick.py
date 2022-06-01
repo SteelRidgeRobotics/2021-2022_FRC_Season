87fb04219a6bd13e42f1dfca996f78bb879ddb2b
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-06-01 08:41:22 -0700

    Update drive_by_joystick.py

--- Larry/commands/drive_by_joystick.py
+++ Larry/commands/drive_by_joystick.py
@@ -2,19 +2,24 @@ import typing
 import commands2
 from subsystems.swerve_drive import SwerveDrive
 import wpilib
-from conversions import *
+import conversions
 
 class DriveByJoystick(commands2.CommandBase):
 
     def __init__(self, drive: SwerveDrive, leftJoyX: typing.Callable[[], float], leftJoyY: typing.Callable[[], float], rightJoyY: typing.Callable[[], float]) -> None:
         super().__init__()
         self.drive = drive
-        self.direction = Conversions.convertJoystickInputToDegrees(leftJoyX(), leftJoyY())
+        
+        self.leftx = leftJoyX
+        self.lefty = leftJoyY
+        self.righty = rightJoyY
+        self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
         self.speed = rightJoyY
 
         self.addRequirements((self.drive))
 
     def execute(self) -> None:
+        self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
         wpilib.SmartDashboard.putNumber("   direction - ", self.direction())
         wpilib.SmartDashboard.putNumber("   speed - ", self.speed())
         self.drive.translate(self.direction, self.speed)
