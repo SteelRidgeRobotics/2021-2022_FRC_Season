71d18cace53210f9c92bbc959308be79d651e259
James <nwalker3.14159@gmail.com>
2022-06-02 15:16:40 -0700

    Made translation on one joystick

--- Larry/commands/drive_by_joystick.py
+++ Larry/commands/drive_by_joystick.py
@@ -3,25 +3,26 @@ import commands2
 from subsystems.swerve_drive import SwerveDrive
 import wpilib
 import conversions
+import math
 
 class DriveByJoystick(commands2.CommandBase):
 
-    def __init__(self, drive: SwerveDrive, leftJoyX: typing.Callable[[], float], leftJoyY: typing.Callable[[], float], rightJoyY: typing.Callable[[], float]) -> None:
+    def __init__(self, drive: SwerveDrive, leftJoyX: typing.Callable[[], float], leftJoyY: typing.Callable[[], float]) -> None:
         super().__init__()
         self.drive = drive
         
         self.leftx = leftJoyX
         self.lefty = leftJoyY
         self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
-        self.speed = rightJoyY
 
         self.addRequirements((self.drive))
 
     def execute(self) -> None:
+        self.magnitude = math.hypot(self.leftx(), self.lefty())
         self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
         wpilib.SmartDashboard.putNumber("   direction - ", self.direction)
-        wpilib.SmartDashboard.putNumber("   speed - ", self.speed())
-        self.drive.translate(self.direction, self.speed())
+        wpilib.SmartDashboard.putNumber("   speed - ", self.magnitude)
+        self.drive.translate(self.direction, self.magnitude)
     
     def end(self, interrupted: bool) -> None:
         self.drive.stopMotors()
