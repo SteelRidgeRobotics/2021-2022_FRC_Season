02e60728086181ea15f1297f7c90b214ec0e91ba
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2022-06-06 07:51:46 -0700

    Update drive_by_joystick.py

--- Larry/commands/drive_by_joystick.py
+++ Larry/commands/drive_by_joystick.py
@@ -7,12 +7,13 @@ import math
 
 class DriveByJoystick(commands2.CommandBase):
 
-    def __init__(self, drive: SwerveDrive, leftJoyX: typing.Callable[[], float], leftJoyY: typing.Callable[[], float]) -> None:
+    def __init__(self, drive: SwerveDrive, leftJoyX: typing.Callable[[], float], leftJoyY: typing.Callable[[], float], rightJoyX: typing.Callable[[], float]) -> None:
         super().__init__()
         self.drive = drive
         
         self.leftx = leftJoyX
         self.lefty = leftJoyY
+        self.turnPower = rightJoyX
         self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
 
         self.addRequirements((self.drive))
@@ -25,7 +26,14 @@ class DriveByJoystick(commands2.CommandBase):
         self.direction = conversions.Conversions.convertJoystickInputToDegrees(self.leftx(), self.lefty())
         wpilib.SmartDashboard.putNumber("   direction - ", self.direction)
         wpilib.SmartDashboard.putNumber("   speed - ", self.magnitude)
-        self.drive.translate(self.direction, self.magnitude)
+        wpilib.SmartDashboard.putNumber("   turn power - ", self.turnPower())
+        
+        if self.magnitude == 0.0 and self.turnPower() != 0.0:
+            self.drive.turnInPlace(self.turnPower())
+        elif self.magnitude != 0.0 and self.turnPower() == 0.0
+            self.drive.translate(self.direction, self.magnitude)
+        else:
+            self.drive.turnWhileMoving(self.direction, self.magnitude, self.turnPower)
     
     def end(self, interrupted: bool) -> None:
         self.drive.stopMotors()
